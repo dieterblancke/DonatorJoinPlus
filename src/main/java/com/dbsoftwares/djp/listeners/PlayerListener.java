381d2125669891d377de932509e79291f1953738
Dieter Blancke <dieter.blancke@outlook.com>
2019-11-01 11:14:23 +0100

    Reformatted all files, added first join message feature

--- src/main/java/com/dbsoftwares/djp/listeners/PlayerListener.java
+++ src/main/java/com/dbsoftwares/djp/listeners/PlayerListener.java
@@ -33,32 +33,40 @@ import java.util.concurrent.TimeUnit;
  * Project: DonatorJoinPlus
  */
 
-public class PlayerListener implements Listener {
+public class PlayerListener implements Listener
+{
 
     private final LoadingCache<UUID, CompletableFuture<Boolean>> loadingCache = CacheBuilder.newBuilder()
-            .expireAfterWrite(15, TimeUnit.SECONDS)
-            .build(new CacheLoader<UUID, CompletableFuture<Boolean>>() {
+            .expireAfterWrite( 15, TimeUnit.SECONDS )
+            .build( new CacheLoader<UUID, CompletableFuture<Boolean>>()
+            {
                 @ParametersAreNonnullByDefault
-                public CompletableFuture<Boolean> load(final UUID uuid) {
-                    return CompletableFuture.supplyAsync(() -> DonatorJoinPlus.i().getStorage().isToggled(uuid));
+                public CompletableFuture<Boolean> load( final UUID uuid )
+                {
+                    return CompletableFuture.supplyAsync( () -> DonatorJoinPlus.i().getStorage().isToggled( uuid ) );
                 }
-            });
+            } );
 
-    public PlayerListener() {
+    public PlayerListener()
+    {
         // Cleanup task, runs every 3 minutes to ensure the PlayerListener loadingCache is cleaned up.
-        new BukkitRunnable() {
+        new BukkitRunnable()
+        {
             @Override
-            public void run() {
-                if (DonatorJoinPlus.i().isDebugMode()) {
-                    DonatorJoinPlus.getLog().debug("Cleaning up loading cache ... [initialSize={}]", loadingCache.size());
+            public void run()
+            {
+                if ( DonatorJoinPlus.i().isDebugMode() )
+                {
+                    DonatorJoinPlus.getLog().debug( "Cleaning up loading cache ... [initialSize={}]", loadingCache.size() );
                 }
                 loadingCache.cleanUp();
 
-                if (DonatorJoinPlus.i().isDebugMode()) {
-                    DonatorJoinPlus.getLog().debug("Successfully up loading cache ... [currentSize={}]", loadingCache.size());
+                if ( DonatorJoinPlus.i().isDebugMode() )
+                {
+                    DonatorJoinPlus.getLog().debug( "Successfully up loading cache ... [currentSize={}]", loadingCache.size() );
                 }
             }
-        }.runTaskTimerAsynchronously(DonatorJoinPlus.i(), 3600, 3600);
+        }.runTaskTimerAsynchronously( DonatorJoinPlus.i(), 3600, 3600 );
     }
 
     @EventHandler
@@ -84,99 +92,123 @@ public class PlayerListener implements Listener {
     {
         final Player player = event.getPlayer();
 
-        DonatorJoinPlus.i().debug("Initializing loading of storage for player " + player.getName() + ".");
+        DonatorJoinPlus.i().debug( "Initializing loading of storage for player " + player.getName() + "." );
 
-        final CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(() -> DonatorJoinPlus.i().getStorage().isToggled(player.getUniqueId()));
-        loadingCache.put(player.getUniqueId(), future);
+        final CompletableFuture<Boolean> future = CompletableFuture.supplyAsync( () -> DonatorJoinPlus.i().getStorage().isToggled( player.getUniqueId() ) );
+        loadingCache.put( player.getUniqueId(), future );
     }
 
     @EventHandler
-    public void onJoin(final PlayerJoinEvent event) {
+    public void onJoin( final PlayerJoinEvent event )
+    {
         final Player p = event.getPlayer();
 
-        final boolean toggled = getToggledStatus(p.getUniqueId());
-        Utils.setMetaData(p, Utils.TOGGLE_KEY, toggled);
+        final boolean toggled = getToggledStatus( p.getUniqueId() );
+        Utils.setMetaData( p, Utils.TOGGLE_KEY, toggled );
 
-        if (!DonatorJoinPlus.i().isDisableJoinMessage()) {
-            event.setJoinMessage(null);
+        if ( !DonatorJoinPlus.i().isDisableJoinMessage() )
+        {
+            event.setJoinMessage( null );
         }
 
-        if (Utils.isVanished(p) || toggled) {
+        if ( Utils.isVanished( p ) || toggled )
+        {
             return;
         }
 
-        DonatorJoinPlus.i().debug("Executing login event for player " + p.getName() + ".");
+        DonatorJoinPlus.i().debug( "Executing login event for player " + p.getName() + "." );
 
-        executeEvent(true, null, p);
+        executeEvent( true, null, p );
     }
 
     @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-        if (!DonatorJoinPlus.i().isDisableQuitMessage()) {
-            event.setQuitMessage(null);
+    public void onQuit( PlayerQuitEvent event )
+    {
+        if ( !DonatorJoinPlus.i().isDisableQuitMessage() )
+        {
+            event.setQuitMessage( null );
         }
 
         final Player p = event.getPlayer();
 
-        if (Utils.isVanished(p) || (boolean) Utils.getMetaData(p, Utils.TOGGLE_KEY, false)) {
+        if ( Utils.isVanished( p ) || (boolean) Utils.getMetaData( p, Utils.TOGGLE_KEY, false ) )
+        {
             return;
         }
-        executeEvent(false, null, p);
-        DonatorJoinPlus.i().debug("Executing logout event for player " + p.getName() + ".");
+        executeEvent( false, null, p );
+        DonatorJoinPlus.i().debug( "Executing logout event for player " + p.getName() + "." );
     }
 
     @EventHandler
-    public void onWorldChange(PlayerChangedWorldEvent event) {
+    public void onWorldChange( PlayerChangedWorldEvent event )
+    {
         final Player p = event.getPlayer();
 
-        if (Utils.isVanished(p) || (boolean) Utils.getMetaData(p, Utils.TOGGLE_KEY, false)) {
+        if ( Utils.isVanished( p ) || (boolean) Utils.getMetaData( p, Utils.TOGGLE_KEY, false ) )
+        {
             return;
         }
-        executeEvent(false, event.getFrom(), p);
-        executeEvent(true, event.getPlayer().getWorld(), p);
+        executeEvent( false, event.getFrom(), p );
+        executeEvent( true, event.getPlayer().getWorld(), p );
     }
 
-    private boolean getToggledStatus(final UUID uuid) {
-        try {
-            final CompletableFuture<Boolean> future = loadingCache.get(uuid);
+    private boolean getToggledStatus( final UUID uuid )
+    {
+        try
+        {
+            final CompletableFuture<Boolean> future = loadingCache.get( uuid );
 
             return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            return DonatorJoinPlus.i().getStorage().isToggled(uuid);
+        }
+        catch ( InterruptedException | ExecutionException e )
+        {
+            return DonatorJoinPlus.i().getStorage().isToggled( uuid );
         }
     }
 
-    private void executeEvent(final boolean join, final World world, final Player p) {
-        final String[] groups = DonatorJoinPlus.i().getPermission().getPlayerGroups(p);
+    private void executeEvent( final boolean join, final World world, final Player p )
+    {
+        final String[] groups = DonatorJoinPlus.i().getPermission().getPlayerGroups( p );
 
-        DonatorJoinPlus.i().debug("List of groups for player " + p.getName() + ": " + Arrays.toString(groups));
+        DonatorJoinPlus.i().debug( "List of groups for player " + p.getName() + ": " + Arrays.toString( groups ) );
 
-        for (RankData data : DonatorJoinPlus.i().getRankData()) {
+        for ( RankData data : DonatorJoinPlus.i().getRankData() )
+        {
             final EventType type = join ? EventType.JOIN : EventType.QUIT;
-            final EventData eventData = (world != null ? data.getWorldEvents() : data.getEvents()).getOrDefault(type, null);
+            final EventData eventData = (world != null ? data.getWorldEvents() : data.getEvents()).getOrDefault( type, null );
 
-            if (eventData == null) {
+            if ( eventData == null )
+            {
                 continue;
             }
 
-            if (DonatorJoinPlus.i().isUsePermissions()) {
-                if (DonatorJoinPlus.i().getPermission().has(p, data.getPermission())) {
-                    DonatorJoinPlus.i().debug("Player " + p.getName() + " has the permission " + data.getPermission() + ", executing event ...");
+            if ( DonatorJoinPlus.i().isUsePermissions() )
+            {
+                if ( DonatorJoinPlus.i().getPermission().has( p, data.getPermission() ) )
+                {
+                    DonatorJoinPlus.i().debug( "Player " + p.getName() + " has the permission " + data.getPermission() + ", executing event ..." );
 
-                    executeEventData(p, eventData, world);
+                    executeEventData( p, eventData, world );
 
-                    if (DonatorJoinPlus.i().getConfiguration().getBoolean("usepriorities")) {
+                    if ( DonatorJoinPlus.i().getConfiguration().getBoolean( "usepriorities" ) )
+                    {
                         break;
                     }
-                } else {
-                    DonatorJoinPlus.i().debug("Player " + p.getName() + " does not have the permission " + data.getPermission() + ".");
                 }
-            } else {
-                if (Utils.contains(groups, data.getName())) {
-                    DonatorJoinPlus.i().debug("Player " + p.getName() + " is in the group " + data.getName() + ", executing event ...");
-                    executeEventData(p, eventData, world);
+                else
+                {
+                    DonatorJoinPlus.i().debug( "Player " + p.getName() + " does not have the permission " + data.getPermission() + "." );
+                }
+            }
+            else
+            {
+                if ( Utils.contains( groups, data.getName() ) )
+                {
+                    DonatorJoinPlus.i().debug( "Player " + p.getName() + " is in the group " + data.getName() + ", executing event ..." );
+                    executeEventData( p, eventData, world );
 
-                    if (DonatorJoinPlus.i().getConfiguration().getBoolean("usepriorities")) {
+                    if ( DonatorJoinPlus.i().getConfiguration().getBoolean( "usepriorities" ) )
+                    {
                         break;
                     }
                 }
@@ -184,30 +216,39 @@ public class PlayerListener implements Listener {
         }
     }
 
-    private void executeEventData(final Player p, final EventData eventData, final World world) {
-        if (eventData.isEnabled()) {
-            final String message = formatString(p, eventData.getMessage());
+    private void executeEventData( final Player p, final EventData eventData, final World world )
+    {
+        if ( eventData.isEnabled() )
+        {
+            final String message = formatString( p, eventData.getMessage() );
 
-            for (String msg : message.split("<nl>")) {
-                if (world != null) {
-                    for (Player player : world.getPlayers()) {
-                        player.sendMessage(msg);
+            for ( String msg : message.split( "<nl>" ) )
+            {
+                if ( world != null )
+                {
+                    for ( Player player : world.getPlayers() )
+                    {
+                        player.sendMessage( msg );
                     }
-                    Bukkit.getConsoleSender().sendMessage(msg);
-                } else {
-                    Bukkit.broadcastMessage(msg);
+                    Bukkit.getConsoleSender().sendMessage( msg );
+                }
+                else
+                {
+                    Bukkit.broadcastMessage( msg );
                 }
             }
 
-            if (eventData.isFirework()) {
-                Utils.spawnFirework(p.getLocation());
+            if ( eventData.isFirework() )
+            {
+                Utils.spawnFirework( p.getLocation() );
             }
 
-            if (eventData.isSoundEnabled() && eventData.getSound() != null) {
-                p.getWorld().playSound(p.getLocation(), eventData.getSound(), 20F, -20F);
+            if ( eventData.isSoundEnabled() && eventData.getSound() != null )
+            {
+                p.getWorld().playSound( p.getLocation(), eventData.getSound(), 20F, -20F );
             }
 
-            if (eventData.isCommandsEnabled() && eventData.getCommands() != null && !eventData.getCommands().isEmpty())
+            if ( eventData.isCommandsEnabled() && eventData.getCommands() != null && !eventData.getCommands().isEmpty() )
             {
                 for ( String command : eventData.getCommands() )
                 {
@@ -221,12 +262,14 @@ public class PlayerListener implements Listener {
         }
     }
 
-    private String formatString(final Player p, String str) {
-        str = str.replace("%player%", p.getName());
-        str = Utils.c(str);
+    private String formatString( final Player p, String str )
+    {
+        str = str.replace( "%player%", p.getName() );
+        str = Utils.c( str );
 
-        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-            str = me.clip.placeholderapi.PlaceholderAPI.setPlaceholders((OfflinePlayer) p, str);
+        if ( Bukkit.getPluginManager().isPluginEnabled( "PlaceholderAPI" ) )
+        {
+            str = me.clip.placeholderapi.PlaceholderAPI.setPlaceholders( (OfflinePlayer) p, str );
         }
         return str;
     }
